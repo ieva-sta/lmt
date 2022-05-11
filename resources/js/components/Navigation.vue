7c6f56d8ee4d3dfb7c8771dddff69802521d92ce
Ieva <ieva.stasjuka@protonmail.com>
2022-05-11 18:32:59 +0300

    Install Vue, create Navigation component.

---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <div class="d-flex w-100 justify-content-between">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <img src="images/icons/navbar-toggler.svg" alt="Navigācija">
+                </button>
+                <div>
+                    <img src="images/logo.svg" alt="LMT logo">
+                </div>
+                <div>
+                    <img src="images/icons/account.svg" alt="Ikona - mants konts">
+                </div>
+            </div>
+            <div class="collapse navbar-collapse" id="navigation">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
